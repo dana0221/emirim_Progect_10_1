204409a7a2ab3be22f070567d11dc43f81f463a2
dana8 <w2008@e-mirim.hs.kr>
2021-07-19 10:09:44 +0900

    Completion SecondActivity

--- app/src/main/java/com/example/emirim_progect_10_1/SecondActivity.java
+++ app/src/main/java/com/example/emirim_progect_10_1/SecondActivity.java
@@ -3,6 +3,8 @@ package com.example.emirim_progect_10_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Button btnCloes = findViewById(R.id.btn_close);
+        btnCloes.setOnClickListener(btnCloesListener);
     }
+
+    View.OnClickListener btnCloesListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            finish();
+        }
+    };
 }
